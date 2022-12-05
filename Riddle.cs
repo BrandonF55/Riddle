4479d0af1998ffa66a90ec81cc32206445502a1c
Student <student@epicodus-3.hsd1.or.comcast.net>
2022-12-05 14:33:36 -0800

    Finish C# logic for randomizing riddle selection
    co-authored-by: Teddy Atkinson <tedddy.atkinson@gmail.com>
    co-authored-by: Bodie Wood <bodiewood@hotmail.com>

--- Riddle.cs
+++ Riddle.cs
@@ -1,56 +1,63 @@
 using System;
 using System.Collections.Generic;
 
-public class Riddle
-{
-  static void Main()
-  {
-    string userAnswer = AskRiddle();
-    GetAnswer(userAnswer);
-  }
+public class Test{
 
-  static int AskRiddle()
+  static void Main(){
+    Dictionary<int, string> riddle = new Dictionary<int, string>();
 
-  {
-    Dictionary<string, string> riddleDict = new Dictionary<string, string>();
-      riddleDict.Add("What gets wet while drying?", 1);
+    riddle.Add(1, "When is a door no longer a door?");
+    riddle.Add(2, "What gets wetter as it dries?");
+    riddle.Add(3, "What tastes better than it smells");
+    riddle.Add(4, "What building has the most stories?"); 
 
-      // if(riddleDictAnswers["What gets wet while drying?"] == "towel")
+    Dictionary<string, string> answer = new Dictionary<string, string>();
 
-      // riddleDict.Add(2, "What gets wet while drying?");
-      // riddleDict.Add(3, "What gets wet while drying?");
-      // riddleDict.Add(4, "What gets wet while drying?");
-      // riddleDict.Add(5, "What gets wet while drying?");
+    answer.Add("When is a door no longer a door?", "ajar");
+    answer.Add("What gets wetter as it dries?", "towel");
+    answer.Add("What tastes better than it smells", "tongue");
+    answer.Add("What building has the most stories?", "library"); 
 
-      // riddleDict.Add(2, "What gets wet while drying?");
-      // riddleDict.Add(3, "What gets wet while drying?");
-      // riddleDict.Add(4, "What gets wet while drying?");
-      // riddleDict.Add(5, "What gets wet while drying?");
+
+    /*MAKE A RANDOM NUMBER*/
+    int count = 0;
+    foreach (var i in riddle){
+      count++;
+    }
+
+    Random rnd = new Random();  //makes a random number?
+    int num = rnd.Next(1,count);  //0-4
+
+    /*GRAB RANDOM RIDDLE*/
+    string chosenRiddle = riddle[num];
+    Console.WriteLine(riddle[num]);
+
+    /*MATCH RIDDLE TO ANSWER*/
+    string userAnswer = Console.ReadLine();
+    if(userAnswer == answer[chosenRiddle]) {
+      Console.WriteLine("GREAT SUCCESS");
+    }
+    
+    
+    
+    //grab random quote using the generated random number
+
     
 
-    Console.WriteLine(riddleDict[1]);
 
-    string userAnswer = Console.ReadLine();
-    return riddleDict[];
+    // foreach (var i in riddle){
+    //   Console.WriteLine(i);
+    // }
+
+    // foreach (var i in riddle){
+    //   Console.WriteLine(i.Key);
+    // }
+
+
+    //1. Create a dictionary full of riddles and values
+    //2. Generate a random number between 1 and the amount of riddles
+    //3. Grab a dictionary key based on that random number
   }
 
-  static string GetAnswer(string answer)
-  {
-    Dictionary<int, string> riddleDictAnswers = new Dictionary<int, string>();
-    riddleDict.Add(1, "towel");
-    string returnString = "";
 
-    if (answer == correctAnswer)
-    {
-      returnString = (correctAnswer + " is correct");
-      Console.WriteLine(returnString);
-      return returnString;
-    }
-    else
-    {
-      returnString = ("Sphinx eats user");
-      Console.WriteLine(returnString);
-      return returnString;
-    }
-  }
 }
